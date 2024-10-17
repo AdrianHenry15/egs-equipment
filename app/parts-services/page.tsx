c8286540d00c1f8f88c3c1e29534e85180c67899
Adrian <adrianhenry2115@gmail.com>
2024-10-17 11:20:11 -0400

    file name change

--- app/parts-services/page.tsx
+++ app/parts-services/page.tsx
@@ -1,4 +1,4 @@
-import ContactForm from "@/components/contact-form/contact-form";
+import ContactForm from "@/components/contact-form";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
