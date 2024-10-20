bc4d85fc3e64a9fea5c27e73ba98c85eb008b2e3
Adrian <adrianhenry2115@gmail.com>
2024-10-20 08:26:21 -0400

    swiper fade in

--- components/products/product-row.tsx
+++ components/products/product-row.tsx
@@ -100,13 +100,7 @@ const ProductRow = (props: IProductRowProps) => {
 
             return (
                 <div key={index} className="flex-shrink-0 p-4">
-                    <Image
-                        src={logoImage}
-                        alt={`${logo} logo`}
-                        width={100}
-                        height={100}
-                        objectFit="contain"
-                    />
+                    <Image src={logoImage} alt={`${logo} logo`} width={100} height={100} />
                 </div>
             );
         });
@@ -129,57 +123,57 @@ const ProductRow = (props: IProductRowProps) => {
             </div>
 
             {/* Products Swiper Carousel */}
-            <Swiper
-                pagination={{ clickable: true }}
-                modules={[Pagination]}
-                className="flex items-center overflow-x-hidden overflow-y-hidden px-6"
-                breakpoints={{
-                    // For mobile screens smaller than 576px (very small mobile)
-                    320: {
-                        slidesPerView: 1,
-                    },
-                    // For mobile screens between 576px and 639px
-                    576: {
-                        slidesPerView: 1.5,
-                    },
-                    // When the window is 640px to 767px (small tablets or large mobile devices)
-                    640: {
-                        slidesPerView: 2,
-                    },
-                    // For screens between 768px and 1023px (medium to large tablets)
-                    768: {
-                        slidesPerView: 2.5,
-                    },
-                    // For screens between 1024px and 1279px (small desktops or laptops)
-                    1024: {
-                        slidesPerView: 3,
-                    },
-                    // For screens between 1280px and 1535px (larger desktops)
-                    1280: {
-                        slidesPerView: 4,
-                    },
-                    // For extra-large screens (1536px and up)
-                    1536: {
-                        slidesPerView: 4.5,
-                    },
-                }}
+            <motion.div
+                variants={itemVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.01 }} // Trigger when 10% of the component is visible
+                transition={{ duration: 0.8, delay: 0.1 }} // Adjust delay for staggered effect
             >
-                {products.map((product: ProductType) => (
-                    <SwiperSlide key={product.id}>
-                        <motion.div
-                            variants={itemVariants}
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.1 }} // Trigger when 10% of the component is visible
-                            transition={{ duration: 0.8, delay: 0.1 }} // Adjust delay for staggered effect
-                        >
+                <Swiper
+                    pagination={{ clickable: true }}
+                    modules={[Pagination]}
+                    className="flex items-center overflow-x-hidden overflow-y-hidden px-6"
+                    breakpoints={{
+                        // For mobile screens smaller than 576px (very small mobile)
+                        320: {
+                            slidesPerView: 1,
+                        },
+                        // For mobile screens between 576px and 639px
+                        576: {
+                            slidesPerView: 1.5,
+                        },
+                        // When the window is 640px to 767px (small tablets or large mobile devices)
+                        640: {
+                            slidesPerView: 2,
+                        },
+                        // For screens between 768px and 1023px (medium to large tablets)
+                        768: {
+                            slidesPerView: 2.5,
+                        },
+                        // For screens between 1024px and 1279px (small desktops or laptops)
+                        1024: {
+                            slidesPerView: 3,
+                        },
+                        // For screens between 1280px and 1535px (larger desktops)
+                        1280: {
+                            slidesPerView: 4,
+                        },
+                        // For extra-large screens (1536px and up)
+                        1536: {
+                            slidesPerView: 4.5,
+                        },
+                    }}
+                >
+                    {products.map((product: ProductType) => (
+                        <SwiperSlide key={product.id}>
                             <div className="flex-shrink-0">
                                 <ProductItem product={product} />
                             </div>
-                        </motion.div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </motion.div>
         </div>
     );
 };
