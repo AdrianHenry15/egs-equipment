7525da9874739b441f49d6d5bc715d49f9620ccc
AdrianHenry15 <adrianhenry2115@gmail.com>
2026-01-16 15:52:28 -0500

    update with flyer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import HomeSplash from "@/app/components/home-splash";
 import PoweredBy from "@/app/components/powered-by";
 import FeaturedProducts from "./components/featured-products/featured-products";
 import FeaturedEvents from "./components/featured-events/featured-events";
+import FlyerSection from "@/components/flyer-section";
+import HomeSplash from "./components/home-splash";
 
 export default function Home() {
     return (
         <div className="flex flex-col justify-between relative w-full">
+            <FlyerSection />
             <HomeSplash />
             <PoweredBy />
             <FeaturedProducts />
